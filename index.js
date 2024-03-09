47ba3f757aa7ccb13ff31f803c7d08094ddf61c7
Evan Lee <evan.lee.615@gmail.com>
2024-03-09 17:14:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(data => renderBooks(data))
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
